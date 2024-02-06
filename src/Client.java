d1e0fbe564038a39b86e4e8aabdcb25141ea508d
ivangoyda <iv.goyda.work@gmail.com>
2024-02-06 13:31:58 +0300

    Add task

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String serverHost = "localhost";
+        int serverPort = 8080;
+
+        try {
+            Socket socket = new Socket(serverHost, serverPort);
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Sending a message to the server
+            out.println("Ivan Goyda");
+
+            // Receiving a response from the server
+            String response = in.readLine();
+            System.out.println("Server response: " + response);
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
